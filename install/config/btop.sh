75e3d8dc9b7710abbac9fd7b1e533ced60e644b3
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-16 10:56:08 -0400

    fix btop config

--- install/config/btop.sh
+++ install/config/btop.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-BTOP_CONFIG=~/.config/btop
-if [ -d $BTOP_CONFIG ]; then
-  mv $BTOP_CONFIG $BTOP_CONFIG-backup-$TIMESTAMP
-fi
-
 BTOP_CONFIG_STATE=~/.local/state/ohmydebn-config/btop-config-20250916
 if [ ! -f $BTOP_CONFIG_STATE ]; then
+  TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+  BTOP_CONFIG=~/.config/btop
+  if [ -d $BTOP_CONFIG ]; then
+    mv $BTOP_CONFIG $BTOP_CONFIG-backup-$TIMESTAMP
+  fi
   ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Configuring btop"
   cp -av ~/.local/share/ohmydebn/config/btop ~/.config/
   touch $BTOP_CONFIG_STATE
