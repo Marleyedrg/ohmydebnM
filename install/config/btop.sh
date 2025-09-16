98cd2d8c3e92fb85d0c8de7e30856d6da2fddf94
Doug Burks <doug.burks@securityonionsolutions.com>
2025-09-16 10:08:51 -0400

    update btop config to support vim motions

--- install/config/btop.sh
+++ install/config/btop.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ ! -d ~/.config/btop ]; then
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+BTOP_CONFIG=~/.config/btop
+if [ -d $BTOP_CONFIG ]; then
+  mv $BTOP_CONFIG $BTOP_CONFIG-backup-$TIMESTAMP
+fi
+
+BTOP_CONFIG_STATE=~/.local/state/ohmydebn-config/btop-config-20250916
+if [ ! -f $BTOP_CONFIG_STATE ]; then
   ~/.local/share/ohmydebn/bin/ohmydebn-headline "cat" "Configuring btop"
   cp -av ~/.local/share/ohmydebn/config/btop ~/.config/
-  echo
+  touch $BTOP_CONFIG_STATE
 fi
 
 BTOP_THEMES_DIR=~/.config/btop/themes
